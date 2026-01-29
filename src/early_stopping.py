bfbf2069f6a5231bb29a2e5b3dc9c82f15b38227
Mahdi2001 <156167981+Mahdi79-physics@users.noreply.github.com>
2026-01-29 17:24:15 +0330

    Create early_stopping.py

---
+++ src/early_stopping.py
@@ -0,0 +1,21 @@
+class EarlyStopping:
+    def __init__(self, patience=100, verbose=False, delta=0):
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.best_score = None
+        self.early_stop = False
+        self.counter = 0
+
+    def __call__(self, val_loss, model=None):
+        if self.best_score is None:
+            self.best_score = val_loss
+        elif val_loss > self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = val_loss
+            self.counter = 0
